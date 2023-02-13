96ee985bc92f9fbefebec4dd7df65fdd0d967359
micserv010821 <dotnet290320@gmail.com>
2023-02-13 19:34:07 +0200

    init

---
+++ maven_demo1/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Person p = new Person("jenny", "14689", 1.8f);
+
+        System.out.println(p);
+    }
+}
